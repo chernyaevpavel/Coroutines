5313faba38ea302b3775ba99941bdcad894ea150
Pavel Chernyaev <pawliklist@yandex.ru>
2024-07-03 07:01:31 +0400

    dto+main

--- src/main/kotlin/dto/Comment.kt
+++ src/main/kotlin/dto/Comment.kt
@@ -1,11 +1,11 @@
 package dto
 
-data class Post(
+data class Comment(
     val id: Long,
+    val postId: Long,
     val authorId: Long,
     val content: String,
     val published: Long,
     val likedByMe: Boolean,
     val likes: Int = 0,
-    var attachment: Attachment? = null,
 )
